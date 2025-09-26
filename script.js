6dc96b4f7e27c8a9dc4b77729cea39d30420330f
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-26 21:50:10 +0600

    add 1st example

---
+++ script.js
@@ -0,0 +1,11 @@
+// =================|Example 01|=============
+class Parents {
+    hello() {
+        console.log("Hello");
+
+    };
+}
+class Child extends Parents {}
+let obj = new Child();
+obj.hello();
+
